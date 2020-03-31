072a9372844f21d58fbb8731242752a6ea5ff8b4
Aidan Farber <farberai31@gmail.com>
2020-03-31 14:16:22 -0400

    3/31 12pm Revision

--- static/scripts.js
+++ static/scripts.js
@@ -5,3 +5,23 @@ $(".nav-link").click(function() {
         scrollTop: $(section).offset().top - 83
     }, 20);
 });
+
+
+// $(window).scroll(function() {
+//     if (isInView("#home")) { window.history.pushState("state", "title", "/"); return; }
+//     if (isInView("#about")) { window.history.pushState("state", "title", "#about"); return; }
+//     if (isInView("#projects")) { window.history.pushState("state", "title", "#projects"); return; }
+// });
+
+// function isInView(elem) {
+//     var $elem = $(elem);
+//     var $window = $(window);
+
+//     var docViewTop = $window.scrollTop();
+//     var docViewBottom = docViewTop + $window.height();
+
+//     var elemTop = $elem.offset().top;
+//     var elemBottom = elemTop + $elem.height();
+
+//     return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+// }
